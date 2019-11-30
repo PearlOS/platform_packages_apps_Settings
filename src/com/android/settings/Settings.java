c2b3a763be2a61e3b418cd6594efbc49098afb20
spezi77 <spezi77@users.noreply.github.com>
2019-11-30 08:22:16 +0000

    Implement Shell to Settings Based on implementations by DU and OmniROM, so credits to mazda and maxwen for this.
    spezi77:
    -Added our new Pixel Dust icon for Ten. Credits to Roger Truttmann!
    -Moved PDSettings into System Settings.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -177,4 +177,5 @@ public class Settings extends SettingsActivity {
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
     public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
+    public static class ShellSettingsActivity extends SettingsActivity { /* empty */ }
 }
